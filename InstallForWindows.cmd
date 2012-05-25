b5d48cbc368f042558300ffc6c775e4cfcc3e76e
Iain Ballard <iain.ballard@7digital.com>
2012-05-25 11:39:54 +0100

    vbox into path

--- InstallForWindows.cmd
+++ InstallForWindows.cmd
@@ -5,3 +5,4 @@ powershell.exe -NoProfile -ExecutionPolicy ByPass ".\Scripts\Windows\InstallRuby
 powershell.exe -NoProfile -ExecutionPolicy ByPass ".\Scripts\Windows\InstallRubyDevKit.ps1"
 powershell.exe -NoProfile -ExecutionPolicy ByPass ".\Scripts\Windows\InstallGems.ps1"
 powershell.exe -NoProfile -ExecutionPolicy ByPass ".\Scripts\Windows\InstallVirtualBox.ps1"
+powershell.exe -NoProfile -ExecutionPolicy ByPass ".\Scripts\Windows\AddPathToVirtualBox.ps1"
